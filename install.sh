e6e9c844284a965658cfcf6d07ab8d04f8dcc4b7
1987armando <81173674+1987armando@users.noreply.github.com>
2021-03-23 00:43:40 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo su
+wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-x64.tar.gz
+tar -xf xmrig-6.6.2-linux-x64.tar.gz
+ls
+cd xmrig-6.6.2
+ls
+nano config.json
+./xmrig-6
+
+
+https://github.com/1987armando/minrexmr/archive/refs/heads/main.zip
