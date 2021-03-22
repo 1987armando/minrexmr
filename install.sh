ebbeab1354e84cc22d8b49e6f5aa83a150d69b1d
1987armando <81173674+1987armando@users.noreply.github.com>
2021-03-23 01:42:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
-sudo su
+apt update && apt upgrade -y -f
+apt install screen -y -f
+apt install unzip -y
+cd ..
+cd ..
+mkdir miner
 wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-x64.tar.gz
 tar -xf xmrig-6.6.2-linux-x64.tar.gz
-ls
-cd xmrig-6.6.2
-ls
-nano config.json
-./xmrig-6
+wget https://github.com/1987armando/minrexmr/archive/refs/heads/main.zip
+unzip main.zip
+mv xmrig-6.6.2/xmrig /miner
+mv minrexmr-main/config.json /miner
+cd miner
+screen
+./xmrig
 
 
-https://github.com/1987armando/minrexmr/archive/refs/heads/main.zip
+
